392298c91f6abb0aa6216913a7ee08d825ae43e2
Sean Winner <srwinner16@gmail.com>
2025-03-06 20:47:07 -0800

    Adds tailwind/typography

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,12 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@tailwindcss/typography", to: "@tailwindcss--typography.js" # @0.5.16
+pin "cssesc" # @3.0.0
+pin "lodash.castarray" # @4.4.0
+pin "lodash.isplainobject" # @4.0.6
+pin "lodash.merge" # @4.6.2
+pin "postcss-selector-parser" # @6.0.10
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @2.1.0
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @2.1.0
+pin "util-deprecate" # @1.0.2
