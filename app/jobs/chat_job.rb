422d3e732d57a1ce15449f0ac53265bbc39d51d2
Sean Winner <srwinner16@gmail.com>
2025-03-15 19:25:50 -0700

    Adds a typing indicator

--- app/jobs/chat_job.rb
+++ app/jobs/chat_job.rb
@@ -4,13 +4,6 @@ class ChatJob < ApplicationJob
   def perform(chat_id, message)
     chat = Chat.find(chat_id)
 
-    # Start with a "typing" indicator
-    Turbo::StreamsChannel.broadcast_append_to(
-      chat,
-      target: "messages",
-      partial: "messages/typing"
-    )
-
     chat.ask(message)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,22 @@
 class Message < ApplicationRecord
   acts_as_message
   
-  after_create_commit :broadcast_message
+  after_create_commit :broadcast_create_message
+  after_update_commit :broadcast_update_message
   
-  def broadcast_message
+  def broadcast_create_message
     broadcast_append_to( 
       [chat, "messages"], 
       target: "messages", 
+      partial: "messages/message", 
+      locals: {message: self},
+    )
+  end
+
+  def broadcast_update_message
+    broadcast_replace_to( 
+      [chat, "messages"], 
+      target: "message_#{id}", 
       partial: "messages/message", 
       locals: {message: self},
     )
